98caabdcd3a4566d812ba1fd1e6ed1784498c75f
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-10-20 23:11:31 +0200

    store should be in a separate folder

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import counterReducer from "../features/counter/counterSlice";
-import postReducer from "../features/posts/postSlice"
+import postReducer from "../../features/posts/postSlice"
 
 
 
